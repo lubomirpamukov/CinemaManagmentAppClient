0f2ed3a1a0859bf7e87be61031a1ea1a475b7779
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-11 18:13:10 +0300

    added login and register routes

--- cinema-client/src/App.tsx
+++ cinema-client/src/App.tsx
@@ -1,11 +1,23 @@
 import { AuthProvider } from "./context/AuthContext";
-import LoginPage from "./pages/LoginPage"
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./App.css";
 
 function App() {
-  return <AuthProvider>
-    <LoginPage />
+  return (
+  <BrowserRouter>
+   <AuthProvider>
+    <Navbar />
+      <Routes>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+      </Routes>
   </AuthProvider>
+  </BrowserRouter>
+ 
+  )
   }
 
 export default App;
