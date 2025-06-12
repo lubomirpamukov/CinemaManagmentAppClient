1ef915ea2b0b21afc333b35cfb6c29a3f17fe89b
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-12 14:26:39 +0300

    added MovieList component

---
+++ cinema-client/src/components/movie/MovieList.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import ShowMovie from "./ShowMovie";
+import styles from "./MovieList.module.css";
+import type { Movie } from "../../validations";
+// to do import spinner;
+
+type MovieListProps = {
+  movies: Movie[];
+  loading: boolean;
+  refresh: () => void;
+};
+
+const MovieList: React.FC<MovieListProps> = ({ movies, refresh, loading }) => {
+  if (loading) {
+    return; // to do spinner;
+  }
+
+  if (!movies || movies.length === 0) {
+    return <div className={styles.emptyMessage}>No movies found.</div>;
+  }
+
+  return (
+    <React.Fragment>
+      <div className={styles.movieList}>
+        {movies.map((movie) => (
+          <ShowMovie key={movie.id} movie={movie} onRefresh={refresh} />
+        ))}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MovieList
