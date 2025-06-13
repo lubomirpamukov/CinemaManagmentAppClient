78a60c6ba54cf042acaf76d2c22bc0b22073861a
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-13 08:57:49 +0300

    added hall validations

---
+++ cinema-client/src/validations/hall.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+// constants
+export const HallValidation = {
+    name: 'Hall name must be between 3 and 100 characters long',
+    layoutRows: 'Rows must be between 1 and 50',
+    layoutColumns: 'Columns must be between 1 and 50',
+    price: 'Price must be a positive number',
+    seats: 'All seats must be within the rows and columns range of the hall layout',
+    seatName: 'Seat name must be between 1 and 10 characters long',
+    movieOverlap: 'Movie times cannot overlap'
+};
+
+//schemas
+export const seatsSchema = z.object({
+  originalSeatId: z.string().optional(),
+  row: z.number(),
+  column: z.number(),
+  seatNumber: z.string().min(1, HallValidation.seatName).max(10, HallValidation.seatName),
+  isAvailable: z.boolean().optional(),
+  type: z.enum(["regular", "vip", "couple"]),
+  price: z.number().min(0, HallValidation.price),
+});
+
+//types
+
+export type TSeat = z.infer<typeof seatsSchema>
