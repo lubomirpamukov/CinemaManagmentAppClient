2088cb4172fce79a9cb331eeb40d43046cabd46b
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-23 12:58:30 +0300

    refactor

--- cinema-client/src/validations/hall.ts
+++ cinema-client/src/validations/hall.ts
@@ -2,13 +2,14 @@ import { z } from "zod";
 
 // constants
 export const HallValidation = {
-    name: 'Hall name must be between 3 and 100 characters long',
-    layoutRows: 'Rows must be between 1 and 50',
-    layoutColumns: 'Columns must be between 1 and 50',
-    price: 'Price must be a positive number',
-    seats: 'All seats must be within the rows and columns range of the hall layout',
-    seatName: 'Seat name must be between 1 and 10 characters long',
-    movieOverlap: 'Movie times cannot overlap'
+  name: "Hall name must be between 3 and 100 characters long",
+  layoutRows: "Rows must be between 1 and 50",
+  layoutColumns: "Columns must be between 1 and 50",
+  price: "Price must be a positive number",
+  seats:
+    "All seats must be within the rows and columns range of the hall layout",
+  seatName: "Seat name must be between 1 and 10 characters long",
+  movieOverlap: "Movie times cannot overlap",
 };
 
 //schemas
@@ -16,7 +17,10 @@ export const seatsSchema = z.object({
   _id: z.string().optional(),
   row: z.number(),
   column: z.number(),
-  seatNumber: z.string().min(1, HallValidation.seatName).max(10, HallValidation.seatName),
+  seatNumber: z
+    .string()
+    .min(1, HallValidation.seatName)
+    .max(10, HallValidation.seatName),
   isAvailable: z.boolean().optional(),
   type: z.enum(["regular", "vip", "couple"]),
   price: z.number().min(0, HallValidation.price),
@@ -24,4 +28,4 @@ export const seatsSchema = z.object({
 
 //types
 
-export type TSeat = z.infer<typeof seatsSchema>
+export type THallSeat = z.infer<typeof seatsSchema>;
