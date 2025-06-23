f4eadab8dd3d61266e72b54f41bbe84eb3d52b14
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-23 12:57:18 +0300

    added create reservation service

---
+++ cinema-client/src/services/reservationService.ts
@@ -0,0 +1,19 @@
+import { type TCreateReservation } from "../validations";
+const BASE_URL = "http://localhost:3123/reservation";
+export const createReservation = async (
+  reservationData: TCreateReservation
+) => {
+  //to do add return type
+  console.log(reservationData)
+  const createReservation = await fetch(BASE_URL, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reservationData),
+  });
+
+  if (!createReservation.ok) throw new Error("Failed to create Reservation.");
+  return createReservation.json();
+};
