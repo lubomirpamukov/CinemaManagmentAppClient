58b3b801adcbf546941246a003a5ff12de9199d5
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-25 13:34:57 +0300

    added strict mode

--- cinema-client/src/App.tsx
+++ cinema-client/src/App.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import { AuthProvider } from "./context/AuthContext";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -5,25 +6,26 @@ import MoviePage from "./pages/MoviesPage";
 import BookingPage from "./pages/BookingPage";
 import MyBookingsPage from "./pages/MyBookingsPage";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-  <BrowserRouter>
-   <AuthProvider>
-    <Navbar />
-      <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/register" element={<RegisterPage/>}/>
-        <Route path="/movies" element={<MoviePage/>}/>
-        <Route path="/booking/:movieId" element={<BookingPage/>}/>
-        <Route path="/bookings" element={<MyBookingsPage/>}/>
-      </Routes>
-  </AuthProvider>
-  </BrowserRouter>
- 
-  )
-  }
+    <StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/movies" element={<MoviePage />} />
+            <Route path="/booking/:movieId" element={<BookingPage />} />
+            <Route path="/bookings" element={<MyBookingsPage />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
+}
 
 export default App;
