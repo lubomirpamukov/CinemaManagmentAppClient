f4ee275566b28441f8afda79d9155d282933f852
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-25 13:36:26 +0300

    added filters

--- cinema-client/src/pages/MyBookingsPage.tsx
+++ cinema-client/src/pages/MyBookingsPage.tsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useUserReservations, useDeleteReservation } from "../hooks";
 import { useAuth } from "../context/AuthContext";
 import styles from "./MyBookings.module.css";
 import Spinner from "../components/Spinner";
 import type { TReservationDisplay } from "../validations";
+import type { TReservationFilters } from "../services/";
 
 const MyBookingsPage: React.FC = () => {
   const { userId } = useAuth();
-  const { reservations, loading, error, refetch } = useUserReservations(userId);
-  const { deleteReservation } = useDeleteReservation()
+  const [filters, setFilters] = useState<TReservationFilters>({});
+  const { reservations, loading, error, refetch } =
+    useUserReservations(filters);
+  const { deleteReservation } = useDeleteReservation();
+
+  useEffect(() => {
+    if (userId) setFilters((prev) => ({ ...prev, userId }));
+  }, [userId]);
+
+  const handleStatusClick = (
+    status: "pending" | "confirmed" | "failed" | "completed"
+  ) => {
+    setFilters((prev) => {
+      const currentStatuses = prev.status || [];
+      // If the status is already selected, remove it. Otherwise, add it.
+      const newStatuses = currentStatuses.includes(status)
+        ? currentStatuses.filter((s) => s !== status)
+        : [...currentStatuses, status];
+
+      return {
+        ...prev,
+        status: newStatuses.length > 0 ? newStatuses : undefined,
+      };
+    });
+  };
+
+  const reservationStatuses: Array<
+    "pending" | "confirmed" | "failed" | "completed"
+  > = ["pending", "confirmed", "failed", "completed"];
 
   const handleDelete = async (reservationId: string) => {
     if (window.confirm("Are you sure you want to delete thi reservation?")) {
@@ -21,6 +49,23 @@ const MyBookingsPage: React.FC = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>My Bookings</h2>
+
+      <div className={styles.filterContainer}>
+        <div className={styles.filterButtons}>
+          {reservationStatuses.map((status) => (
+            <button
+              key={status}
+              onClick={() => handleStatusClick(status)}
+              className={`${styles.filterButton} ${
+                filters.status?.includes(status) ? styles.active : ""
+              }`}
+            >
+              {status.charAt(0).toUpperCase() + status.slice(1)}
+            </button>
+          ))}
+        </div>
+      </div>
+
       {loading && <Spinner />}
       {error && <div className={styles.error}>{error}</div>}
       <div className={styles.list}>
