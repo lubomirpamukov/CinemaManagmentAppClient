118d90f3911cf286362f4058375e1975ea107944
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-28 21:16:47 +0300

    added sidebar profile component

---
+++ cinema-client/src/components/user/ProfileSidebar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./ProfileSidebar.module.css";
+
+const ProfileSidebar: React.FC = () => {
+  return (
+    <nav className={styles.sidebar}>
+      <ul className={styles.navList}>
+        
+        <li className={styles.navItem}>
+          <NavLink
+            to="/my-profile/details"
+            className={({ isActive }) =>
+              isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+            }
+          >
+            Update Details
+          </NavLink>
+        </li>
+        <li className={styles.navItem}>
+          <NavLink
+            to="/my-profile/password"
+            className={({ isActive }) =>
+              isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+            }
+          >
+            Change Password
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default ProfileSidebar;
